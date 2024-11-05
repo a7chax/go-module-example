aa71e584a098ec12279f9f3f1628039c17aa8ead
telkomdev-arung <akuarung83@gmail.com>
2024-11-05 16:49:51 +0700

    add with parameter

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/a7chax/go-module-example
+module github.com/a7chax/go-module-example/v2
 
 go 1.15
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package go_say_hello
 
-func SayHello() string {
-	return "Hello, Good People!"
+func SayHello(name string) string {
+	return "Hello, Good People! " + name
 }
